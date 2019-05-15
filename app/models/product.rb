d05b28461bc0dd213719b6e73d212b6a4442244a
Julio Bertty <julio.bertty@gmail.com>
2019-05-15 15:37:39 -0500

    CRUD complete!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
+
+  validates :name, :price, :category_ids, presence: true
+  #validates :price, numericality: { only_integer: true }
 end
